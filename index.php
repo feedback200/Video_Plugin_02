327aa8575e0d3e76e0a22059f9ee58592969a854
feedback200 <155993830+feedback200@users.noreply.github.com>
2024-07-02 13:23:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
+// Requiere el archivo que contiene la definición de la clase VideoJsViewerPlugin
 require_once('VideoJsViewerPlugin.inc.php');
 
+// Crea una nueva instancia del plugin VideoJsViewerPlugin y la devuelve
 return new VideoJsViewerPlugin();
 
 ?>
+
